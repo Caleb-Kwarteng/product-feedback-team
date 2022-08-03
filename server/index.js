930a8370dd6c9367c86fbed4c89fbd2434e07f14
hassanatu <ahmed.hassanatu@amalitech.org>
2022-08-03 07:28:04 +0000

    initial commit

--- server/index.js
+++ server/index.js
@@ -8,8 +8,62 @@ app.use(express.json());
 
 
 //Routes//
+app.get("/",(req,res)=>{
+    res.send("Testing Project.......00000..................");
+
+});
 
 //Create a feedback
+
+app.post("/products-request",async (req,res)=>{
+  
+
+    try{
+        const {title, category,description}=req.body;
+     
+        const newFeedback=await pool.query('INSERT INTO product_features (title,category,description) VALUES ($1,$2,$3) RETURNING *', 
+        [title,category,description],(error,results)=>{
+            if(error){
+                res.send(error.message);
+
+                throw error;
+            }
+            res.json(newFeedback.row[0]);
+            res.status(201).send(`request added`)
+
+        });
+
+    }catch(err){
+        res.send(err.message);
+    }
+
+});
+
+app.path("/update-feedback",(req,res)=>{
+    const feedbackTitle =req.body.feedbackTitle;
+    const category = req.body.category;
+    const detail = req.body.detail;
+
+});
+
+app.post("/category",(req,res)=>{
+    const category = req.body.category;
+    
+
+});
+
+app.get("/roadmap",(req,res)=>{
+    //title
+    //category
+    //status
+    //number of comment
+    //number of reply
+
+});
+
+app.get("/get-all-feedback",(req,res)=>{
+
+});
 
 //Get a feedback
 
