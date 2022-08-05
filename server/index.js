1cb03f40a0db7c3a8ed58d57dd5b1d819672723b
hassanatu <ahmed.hassanatu@amalitech.org>
2022-08-05 08:05:43 +0000

    working on endpoints

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const pool = require("./db");
+const { query } = require("express");
 //Set Up Middleware
 app.use(cors());
 app.use(express.json());
@@ -9,27 +10,47 @@ app.use(express.json());
 
 //Routes//
 app.get("/",(req,res)=>{
-    res.send("Testing Project.......00000..................");
+    res.send("Hello, from product feedback");
 
 });
 
 //Create a feedback
 
-app.post("/products-request",async (req,res)=>{
-  
-
+const addUser = async(req,res)=>{
     try{
-        const {title, category,description}=req.body;
+        const {id,name,username,image,password} = req.body;
+        await pool.query('INSERT INTO users (id,name,username,image,password) VALUES ($1,$2,$3,$4,$5) RETURNING *',
+        [id,name,username,image,password],(error,result)=>{
+            if(error){
+                res.send(error.message);
+                throw error;
+
+            }
+           
+            res.status(200).send("new user added");
+
+        });
+
+    }catch(error){
+        res.send(error.message);
+    }
+
+}
+
+
+const productRequest = async (req,res)=>{
+    try{
+        const {id,title, category,description,status,upvotes}=req.body;
      
-        const newFeedback=await pool.query('INSERT INTO product_features (title,category,description) VALUES ($1,$2,$3) RETURNING *', 
-        [title,category,description],(error,results)=>{
+        await pool.query('INSERT INTO product_features (id,title,category,description,status,upvotes) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *', 
+        [id,title,category,description,status,upvotes],(error,results)=>{
             if(error){
                 res.send(error.message);
 
                 throw error;
             }
-            res.json(newFeedback.row[0]);
-            res.status(201).send(`request added`)
+            
+            res.status(201).send("request added successfully");
 
         });
 
@@ -37,20 +58,44 @@ app.post("/products-request",async (req,res)=>{
         res.send(err.message);
     }
 
-});
+}
 
-app.path("/update-feedback",(req,res)=>{
-    const feedbackTitle =req.body.feedbackTitle;
-    const category = req.body.category;
-    const detail = req.body.detail;
+//creating feedback
 
-});
+const createComment =async(req,res)=>{
+    const {id,content,user_id} =req.body;
+    await pool.query('INSERT INTO comments (id,content,user_id) VALUES ($1,$2,$3) RETURNING *',
+    [id,content,user_id],(error,result)=>{
+        if(error){
+            res.send(error.message);
+            throw error;
+        }
+        res.status(201).send("comment add successfuly");
 
-app.post("/category",(req,res)=>{
-    const category = req.body.category;
-    
+    });
+}
 
-});
+//editting feedback
+
+const editFeedback =  (req,res)=>{
+    const id = parseInt(req.params.id);
+   const {title,category,status,description,upvotes} = req.body;
+   pool.query('UPDATE product_features SET title = $1, category = $2 , status = $3, description = $4 ,upvotes =$5 WHERE id = $6',
+   [title,category,status,description,upvotes],(error,result)=>{
+    if (error){
+        res.send(error.message);
+        throw error;
+    }
+    res.status(200).send(`Feedback modified with ID: ${id}`);
+
+   });
+
+
+}
+
+
+
+
 
 app.get("/roadmap",(req,res)=>{
     //title
@@ -72,6 +117,12 @@ app.get("/get-all-feedback",(req,res)=>{
 //Edit Feedback
 
 //Delete a feedback
+
+//routes
+app.post("/create-comment",createComment);
+app.post("/products-request",productRequest);
+app.post("/add-user",addUser);
+app.put("/edit-feedback/:id",editFeedback);
 
 
 
