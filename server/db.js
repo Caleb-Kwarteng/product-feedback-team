53e44859eb3ae1144f57f7937c8cef5e79538337
eddieDjirackor <edward.djirackor@amalitech.org>
2022-08-11 17:44:06 +0000

    Initial database design

--- server/db.js
+++ server/db.js
@@ -1,11 +1,34 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "12345",
+//     host: "127.0.0.1",
+//     port: 5432,
+//     database: "product-feedback"
+// });
+
+// module.exports = pool;
+
+const Pool = require('pg').Pool;
+
+//configures the db we are connecting to
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "root",
+//     database: "product-feedback",
+//     host: "localhost",
+//     port: 5432
+// });
+
+// module.exports = pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "12345",
-    host: "127.0.0.1",
-    port: 5432,
-    database: "productfeedback"
+    password: "root",
+    database: "hass-prod-feed",
+    host: "localhost",
+    port: 5432
 });
 
 module.exports = pool;
