0645cee22443feb6aa4188cd8ab6d8741740c7bd
eddieDjirackor <edward.djirackor@amalitech.org>
2022-08-13 12:31:34 +0000

    handling error in the hash function

--- server/hash-function.js
+++ server/hash-function.js
@@ -1,12 +1,13 @@
 const bcrypt = require("bcrypt");
 
-const passwordHash = async (password, saltRounds) => {
+const saltRounds = 6;
+
+const passwordHash = async (password) => {
     try {
       const salt = await bcrypt.genSalt(saltRounds);
       return await bcrypt.hash(password, salt);
     } catch (err) {
-        console.log(err);
-    //   return err;
+      return err;
     }
     // return null;
   };
