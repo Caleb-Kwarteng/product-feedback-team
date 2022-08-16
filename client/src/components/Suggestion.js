79dcf63d06c900c802daea47cc6ea31f4f5c617a
Caleb-Kwarteng <caleb.kwarteng@amalitech.org>
2022-08-16 08:52:04 +0000

    Suggestion page

--- client/src/components/Suggestion.js
+++ client/src/components/Suggestion.js
@@ -1,5 +1,19 @@
-const Suggestion = () => {
-  return <></>;
+const Suggestion = ({ feedback }) => {
+  return (
+    <>
+      <div className="singleSuggestion">
+        <div>
+          <h1>{feedback.title}</h1>
+        </div>
+        <div>
+          <h1>{feedback.title}</h1>
+        </div>
+        <div>
+          <h1>{feedback.title}</h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Suggestion;
--- client/src/components/suggestionsPage/SuggestionsPage.js
+++ client/src/components/suggestionsPage/SuggestionsPage.js
@@ -4,34 +4,47 @@ import EnhancementBtn from "../buttons/EnhancementBtn";
 import FeatureBtn from "../buttons/FeatureBtn";
 import UIBtn from "../buttons/UIBtn";
 import UXBtn from "../buttons/UXBtn";
+import Suggestion from "../Suggestion";
 import Board from "./sharedComponents/Board";
 import Header from "./sharedComponents/Header";
 import Roadmap from "./sharedComponents/Roadmap";
 
+import { useEffect, useState } from "react";
+
 const SuggestionsPage = () => {
+  const [feedbacks, setFeedbacks] = useState([]);
+  const getProductFeatures = async () => {
+    try {
+      const response = await fetch("http://localhost:4001/get-feedback");
+      const jsonData = await response.json();
+      setFeedbacks(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  useEffect(() => {
+    getProductFeatures();
+  }, []);
+
+  console.log(feedbacks);
+
   return (
     <>
-      <div
-        className="container-fluid"
-        style={{
-          backgroundCOlor: "background: #F2F2F2;",
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      >
-        <div className="suggestionsLeft">
-          <Board />
-
-          <div className="navDiv p-3">
+      <div>
+        <Board />
+        <nav>
+          <div className="navDiv p-2 ">
             <div>
+              {" "}
               <AllBtn />
             </div>
-
             <div>
+              {" "}
               <UIBtn />
             </div>
 
             <div>
+              {" "}
               <UXBtn />
             </div>
 
@@ -47,13 +60,17 @@ const SuggestionsPage = () => {
               <FeatureBtn />
             </div>
           </div>
+        </nav>
+        <Roadmap />
+      </div>
+      <div>
+        <Header />
 
-          <div className="raodmap">
-            <Roadmap />
-          </div>
-        </div>
-        <div className="suggestionsRight">
-          <Header />
+        <div>
+          {" "}
+          {feedbacks.map((feedback) => {
+            <Suggestion key={feedback.id} feedback={feedback} />;
+          })}
         </div>
       </div>
     </>
@@ -61,3 +78,59 @@ const SuggestionsPage = () => {
 };
 
 export default SuggestionsPage;
+/**
+ *  <div class="container- text-center">
+        <div class="row">
+          <div class="col-4">
+            <div className="suggestionsLeft">
+              <Board />
+
+              <div className="navDiv p-3">
+                <div>
+                  <AllBtn />
+                </div>
+
+                <div>
+                  <UIBtn />
+                </div>
+
+                <div>
+                  <UXBtn />
+                </div>
+
+                <div>
+                  <EnhancementBtn />
+                </div>
+
+                <div>
+                  <BugBtn />
+                </div>
+
+                <div>
+                  <FeatureBtn />
+                </div>
+              </div>
+
+              <div className="raodmap">
+                <Roadmap />
+              </div>
+            </div>
+          </div>
+          <div class="col-8">
+            {" "}
+            <Header />
+            <Suggestion />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="container-fluid"
+        style={{
+          background: "#F7F8FD",
+          display: "flex",
+          justifyContent: "space-around",
+          position: "relative",
+        }}
+      ></div>
+ */
